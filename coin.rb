a1addb53db458e45e8c904378a23aef6a9b3a3c1
Ricky Collins <richardcollins52317@gmail.com>
2018-07-03 15:14:09 -0400

    pass, test for 100 cents returns 4 quarters

--- coin.rb
+++ coin.rb
@@ -1,23 +1,24 @@
 def min_coins(cents)
     used = {}
-    count = 0
+    q_count = 0; d_count = 0; n_count = 0; p_count = 0
     coins = {:quarter => 25, :dime => 10, :nickel => 5, :penny => 1}
     until cents == 0
-        coins.each do |c|
-            if cents < c[1]
-                cents += c[1]
-            else
-                if c[0] == :dime
-                    count = 0
-                elsif c[0] == :nickel
-                    count = 0
-                elsif c[0] == :penny
-                    count = 0
-                end
-                count += 1
-                used.store(c[0], count)
-            end
-            cents -= c[1]
+        if cents >= coins[:quarter]
+            cents -= coins[:quarter]
+            q_count += 1
+            used.store(:quarter, q_count)
+        elsif cents >= coins[:dime]
+            cents -= coins[:dime]
+            d_count += 1
+            used.store(:dime, d_count)
+        elsif cents >= coins[:nickel]
+            cents -= coins[:nickel]
+            n_count += 1
+            used.store(:nickel, n_count)
+        elsif cents >= coins[:penny]
+            cents -= coins[:penny]
+            p_count += 1
+            used.store(:penny, p_count)
         end
     end
     used
